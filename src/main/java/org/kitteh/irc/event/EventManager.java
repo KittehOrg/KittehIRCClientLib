d1b76c1bdeabab05fca177a29f295904dcc33543
mbax <matt@phozop.net>
2014-03-12 16:48:09 -0400

    Document event manager

--- src/main/java/org/kitteh/irc/event/EventManager.java
+++ src/main/java/org/kitteh/irc/event/EventManager.java
@@ -15,31 +15,42 @@ import java.util.concurrent.ConcurrentHashMap;
 public final class EventManager {
     private final Map<Class<?>, Set<Pair<Object, Method>>> registeredEvents = new ConcurrentHashMap<>();
 
-    public void registerEvents(Object object) {
+    /**
+     * Registers any non-static methods annotated with
+     * {@link org.kitteh.irc.event.EventHandler}, provided they have a single
+     * parameter. This parameter is the event.
+     * <p/>
+     * The class listened to must be the same class called in
+     * {@link #callEvent(Object)}, not a super or subclass.
+     *
+     * @param listener listener in which to register events
+     */
+    public void registerEventListener(Object listener) {
         try {
-            Method[] methods = object.getClass().getDeclaredMethods();
+            Method[] methods = listener.getClass().getDeclaredMethods();
             for (Method method : methods) {
-                if (Modifier.isStatic(method.getModifiers())) {
-                    continue;
-                }
-                EventHandler handler = method.getAnnotation(EventHandler.class);
-                if (handler == null) {
+                if (Modifier.isStatic(method.getModifiers()) || method.getAnnotation(EventHandler.class) == null) {
                     continue;
                 }
                 Class<?>[] types = method.getParameterTypes();
                 if (types.length != 1) {
-                    System.out.println("Event handlers must only have one parameter. Invalid method " + method.getName());
+                    System.out.println("Event handlers must only have one parameter. Invalid method " + method.getName() + " in class " + listener.getClass().getSimpleName());
                     continue;
                 }
                 method.setAccessible(true);
-                this.getSet(types[0]).add(new Pair<>(object, method));
+                this.getSet(types[0]).add(new Pair<>(listener, method));
             }
         } catch (Exception e) {
-            System.out.println("Exception registering " + object.getClass().getSimpleName() + ":");
+            System.out.println("Exception registering " + listener.getClass().getSimpleName() + ":");
             e.printStackTrace();
         }
     }
 
+    /**
+     * Calls an event.
+     *
+     * @param event event to call
+     */
     public void callEvent(Object event) {
         Set<Pair<Object, Method>> set = this.registeredEvents.get(event.getClass());
         if (set != null) {
