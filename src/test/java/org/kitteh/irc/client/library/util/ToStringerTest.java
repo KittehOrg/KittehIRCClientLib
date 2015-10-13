e6aa73b92f303643f240fca7d1ed5bbf588d8840
mbax <matt@phozop.net>
2015-10-13 16:27:23 -0400

    Improve ToStringer coverage

---
+++ src/test/java/org/kitteh/irc/client/library/util/ToStringerTest.java
@@ -0,0 +1,44 @@
+package org.kitteh.irc.client.library.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests ToStringer.
+ */
+public class ToStringerTest {
+    /**
+     * Tests a valid toStringer input.
+     */
+    @Test
+    public void toStringer() {
+        Assert.assertNotNull(new ToStringer(this)
+                .add("boolean", true)
+                .add("byte", (byte) 0x00)
+                .add("char", 'c')
+                .add("double", 1D)
+                .add("float", 2F)
+                .add("int", 3)
+                .add("long", 4L)
+                .add("object", new Object())
+                .add("array", new String[]{"arr", "I'm a pirate"})
+                .add("short", (short) 5)
+                .toString());
+    }
+
+    /**
+     * Tests a null addition parameter.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void toStringerNullAddition() {
+        new ToStringer(this).add(null, "test");
+    }
+
+    /**
+     * Tests a null constructor parameter.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void toStringerNullConstructor() {
+        new ToStringer(null);
+    }
+}
