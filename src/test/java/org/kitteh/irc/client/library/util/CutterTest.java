973359bc011a249794611e299ebf9c7813e68e2c
mbax <matt@phozop.net>
2016-02-28 12:25:49 -0500

    Improve Cutter testing

--- src/test/java/org/kitteh/irc/client/library/util/CutterTest.java
+++ src/test/java/org/kitteh/irc/client/library/util/CutterTest.java
@@ -14,15 +14,14 @@ public class CutterTest {
      */
     @Test
     public void cut() {
-        Cutter cutter = new Cutter.DefaultWordCutter();
-        List<String> output = cutter.split("1 1 1 Meow meow mreow purrrrrr hisssssssss", 5);
+        List<String> output = new Cutter.DefaultWordCutter().split("purrrrrrr 1 1 1 Meow meow mreow hisssssssss", 5);
         Assert.assertEquals(9, output.size());
-        Assert.assertEquals("1 1 1", output.get(0));
-        Assert.assertEquals("Meow", output.get(1));
-        Assert.assertEquals("meow", output.get(2));
-        Assert.assertEquals("mreow", output.get(3));
-        Assert.assertEquals("purrr", output.get(4));
-        Assert.assertEquals("rrr", output.get(5));
+        Assert.assertEquals("purrr", output.get(0));
+        Assert.assertEquals("rrrr", output.get(1));
+        Assert.assertEquals("1 1 1", output.get(2));
+        Assert.assertEquals("Meow", output.get(3));
+        Assert.assertEquals("meow", output.get(4));
+        Assert.assertEquals("mreow", output.get(5));
         Assert.assertEquals("hisss", output.get(6));
         Assert.assertEquals("sssss", output.get(7));
         Assert.assertEquals("s", output.get(8));
@@ -33,9 +32,33 @@ public class CutterTest {
      */
     @Test
     public void cutShort() {
-        Cutter cutter = new Cutter.DefaultWordCutter();
-        List<String> output = cutter.split("Hello world!", 15);
+        List<String> output = new Cutter.DefaultWordCutter().split("Hello world!", 15);
         Assert.assertEquals(1, output.size());
         Assert.assertEquals("Hello world!", output.get(0));
+    }
+
+    /**
+     * Tests ability to fail.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void cutFailNegative() {
+        new Cutter.DefaultWordCutter().split("Hi", -2);
+    }
+
+    /**
+     * Tests ability to fail.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void cutFailNull() {
+        new Cutter.DefaultWordCutter().split(null, 2);
+    }
+
+    /**
+     * Spaaaaaaaaace!
+     */
+    @Test
+    public void cutSpace() {
+        List<String> output = new Cutter.DefaultWordCutter().split("                     ", 3);
+        Assert.assertEquals(0, output.size());
     }
 }
