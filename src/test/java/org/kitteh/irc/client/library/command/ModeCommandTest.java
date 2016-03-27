0b15db2f59d6f9c8eb3ef43b78e0166ecd2bf9d5
mbax <matt@phozop.net>
2016-03-27 13:25:37 -0400

    More thorough, more fixed, more modes

--- src/test/java/org/kitteh/irc/client/library/command/ModeCommandTest.java
+++ src/test/java/org/kitteh/irc/client/library/command/ModeCommandTest.java
@@ -7,6 +7,7 @@ import org.kitteh.irc.client.library.element.Channel;
 import org.kitteh.irc.client.library.element.ChannelMode;
 import org.kitteh.irc.client.library.element.ChannelUserMode;
 import org.kitteh.irc.client.library.element.User;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 
 import javax.annotation.Nonnull;
@@ -50,12 +51,27 @@ public class ModeCommandTest {
         Mockito.verify(clientMock, Mockito.times(1)).sendRawLine("MODE #test +A");
     }
 
+    @Test
+    public void testWithFourSimpleModeChanges() {
+        Client clientMock = Mockito.mock(Client.class);
+
+        ModeCommand sut = new ModeCommand(clientMock, this.getMockedChannel(clientMock, "#test"));
+        sut.add(true, this.getChannelMode('A', clientMock, ChannelMode.Type.D_PARAMETER_NEVER));
+        sut.add(true, this.getChannelMode('B', clientMock, ChannelMode.Type.D_PARAMETER_NEVER));
+        sut.add(true, this.getChannelMode('C', clientMock, ChannelMode.Type.D_PARAMETER_NEVER));
+        sut.add(true, this.getChannelMode('D', clientMock, ChannelMode.Type.D_PARAMETER_NEVER));
+        sut.execute();
+        InOrder inOrder = Mockito.inOrder(clientMock, clientMock);
+        inOrder.verify(clientMock, Mockito.times(1)).sendRawLine("MODE #test +ABC");
+        inOrder.verify(clientMock, Mockito.times(1)).sendRawLine("MODE #test +D");
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void testWithOneSimpleModeChangeButWrongClient() {
         Client clientMock = Mockito.mock(Client.class);
 
         ModeCommand sut = new ModeCommand(clientMock, this.getMockedChannel(clientMock, "#test"));
-        ChannelMode mode = this.getChannelMode('A', clientMock, ChannelMode.Type.A_MASK);
+        ChannelMode mode = this.getChannelMode('A', Mockito.mock(Client.class), ChannelMode.Type.A_MASK);
         sut.add(true, mode);
     }
 
