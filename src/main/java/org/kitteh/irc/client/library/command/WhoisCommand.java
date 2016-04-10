a7d78ae292283833364964ef96a94997f7641d4d
mbax <matt@phozop.net>
2016-04-10 14:48:47 -0400

    Add WhoisCommand

---
+++ src/main/java/org/kitteh/irc/client/library/command/WhoisCommand.java
@@ -0,0 +1,73 @@
+/*
+ * * Copyright (C) 2013-2016 Matt Baxter http://kitteh.org
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.kitteh.irc.client.library.command;
+
+import org.kitteh.irc.client.library.Client;
+import org.kitteh.irc.client.library.util.Sanity;
+import org.kitteh.irc.client.library.util.ToStringer;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Sends a WHOIS request to the server.
+ */
+public class WhoisCommand extends Command {
+    private String target;
+
+    /**
+     * Constructs the command.
+     *
+     * @param client the client
+     * @throws IllegalArgumentException if client is null
+     */
+    public WhoisCommand(@Nonnull Client client) {
+        super(client);
+    }
+
+    /**
+     * Sets the target nickname for the command.
+     *
+     * @param target target nick
+     * @return this command
+     * @throws IllegalArgumentException for invalid target
+     */
+    @Nonnull
+    public WhoisCommand target(@Nonnull String target) {
+        this.target = Sanity.safeMessageCheck(target, "target");
+        return this;
+    }
+
+    @Override
+    public void execute() {
+        if (this.target == null) {
+            throw new IllegalStateException("Target not defined");
+        }
+        this.getClient().sendRawLine("WHOIS " + this.target);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringer(this).add("target", this.target).toString();
+    }
+}
