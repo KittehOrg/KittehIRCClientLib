b12009a9ec1779bd5d3a5d0640eed294435e7e57
lol768 <lol768@lol768.com>
2016-10-25 21:46:52 +0100

    Explain what the client states mean

--- src/main/java/org/kitteh/irc/client/library/feature/sts/STSClientState.java
+++ src/main/java/org/kitteh/irc/client/library/feature/sts/STSClientState.java
@@ -27,9 +27,43 @@ package org.kitteh.irc.client.library.feature.sts;
  * Enum to keep track of the STS implementation's state machine.
  */
 public enum STSClientState {
+    /**
+     * A state is not yet available. Perhaps the CAP LS command has not yet been issued or
+     * the IRCd does not support IRCv3.
+     */
     UNKNOWN,
+
+    /**
+     * The CAP LS command gave us back a response, but `sts` wasn't present in the list.
+     */
     NO_STS_PRESENT,
+
+    /**
+     * We connected over an insecure link and found a valid `sts` capability. Now we're
+     * going to reconnect on the specified secure port.
+     */
     STS_PRESENT_RECONNECTING,
+
+    /**
+     * We connected and found a valid STS policy, but we were already connected on a secure
+     * port so we don't need to reconnect. We will update the stored policy, though.
+     */
     STS_PRESENT_ALREADY_SECURE,
+
+
+    /**
+     * We connected via an insecure communication means and found an STS policy. We then
+     * tried to establish a secure connection on the advertised port, but couldn't. This
+     * could be due to a network issue or a problem validating the provided certificate.
+     *
+     * The client will stay in this state and it is not possible to bypass.
+     */
+    STS_PRESENT_CANNOT_CONNECT,
+
+    /**
+     * An STS policy was present when we connected with the insecure port, but it wasn't there
+     * when we connected on the advertised secure port. This is indicative of an active MitM or
+     * a server misconfiguration and should be investigated.
+     */
     INVALID_STS_MISSING_ON_RECONNECT
 }
