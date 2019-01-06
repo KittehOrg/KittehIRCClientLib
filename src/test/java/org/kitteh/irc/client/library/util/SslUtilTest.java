11c362f0480fcdcb4e79f35d4be6365e63ed3d2f
mbax <matt@phozop.net>
2019-01-05 22:34:05 -0500

    Test SslUtil

---
+++ src/test/java/org/kitteh/irc/client/library/util/SslUtilTest.java
@@ -0,0 +1,38 @@
+package org.kitteh.irc.client.library.util;
+
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.net.ssl.TrustManagerFactory;
+
+/**
+ * Tests the SSLUtil class.
+ */
+public class SslUtilTest {
+    /**
+     * Test the isInsecure function.
+     */
+    @Test
+    public void testSecure1() {
+        Assert.assertTrue(SslUtil.isInsecure(InsecureTrustManagerFactory.INSTANCE));
+    }
+
+    /**
+     * Test the isInsecure function.
+     */
+    @Test
+    public void testSecure2() {
+        Assert.assertTrue(SslUtil.isInsecure(new AcceptingTrustManagerFactory()));
+    }
+
+    /**
+     * Test the isInsecure function.
+     *
+     * @throws Exception Oh no
+     */
+    @Test
+    public void testSecure3() throws Exception {
+        Assert.assertFalse(SslUtil.isInsecure(TrustManagerFactory.getInstance("PKIX")));
+    }
+}
