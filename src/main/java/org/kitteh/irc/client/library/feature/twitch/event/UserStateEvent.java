419cda35937f8c22415010ad5291875956f6ea19
mbax <matt@phozop.net>
2021-01-01 01:38:48 -0500

    Happy New Year!

--- src/main/java/org/kitteh/irc/client/library/feature/twitch/event/UserStateEvent.java
+++ src/main/java/org/kitteh/irc/client/library/feature/twitch/event/UserStateEvent.java
@@ -1,5 +1,5 @@
 /*
- * * Copyright (C) 2013-2020 Matt Baxter https://kitteh.org
+ * * Copyright (C) 2013-2021 Matt Baxter https://kitteh.org
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
